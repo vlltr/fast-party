f28645ef3578ad865856add84587e7903b81e2c2
Mauricio Villatoro <mauripr26@gmail.com>
2023-05-01 21:27:41 +0000

    add result model and migrate

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -10,4 +10,9 @@ class Test extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function result()
+    {
+        return $this->belongsTo(Result::class);
+    }
 }
---
+++ database/migrations/2023_05_01_211730_create_results_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Test;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Test::class)->constrained();
+            $table->string('part_number');
+            $table->string('serial_number');
+            $table->integer('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('results');
+    }
+};
